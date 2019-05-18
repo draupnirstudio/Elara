25b0b6396f8c3d45db2d46cb87ac89446a421c85
Joz <jozdortraz@gmail.com>
2019-05-18 23:25:42 +0800

    feat(dev-environment): Add Production docker-compose
    1. Add production docker-compose.yml
    2. Add socket support

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,21 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
 import * as helmet from 'helmet';
+import * as http from 'http';
 
-// Create a new express application instance
 const app: express.Application = express();
 const port = 5000;
 
 app.use(morgan('combined'));
 app.use(helmet());
 
+const server = new http.Server(app);
+const io = require('socket.io')(server);
 
-app.listen(port, () => {
-    console.log(`Elara app listening on port ${port}!`);
+io.on('connection', (socket: any) => {
+    console.log('a user connected');
 });
 
+server.listen(port, () => {
+    console.log(`Elara app listening on port ${port}!`);
+});
